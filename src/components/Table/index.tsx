2197f7262e3ac5d1ed30cabcdddcb41c3e010065
markoclimako <mkclimako@gmail.com>
2023-08-10 17:40:43 -0300

    feat: implement Table component to display planet's infos

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,55 @@
+import { useContext } from 'react';
+import { PlanetContext } from '../../context/planetsContext';
+import { Planet } from '../../types';
+
+export default function Table() {
+  const { planets } = useContext(PlanetContext);
+  const tableHeaders = [
+    'Name',
+    'Rotation Period',
+    'Orbital Period',
+    'Diameter',
+    'Climate',
+    'Gravity',
+    'Terrain',
+    'Surface Water',
+    'Population',
+    'Films',
+    'Created',
+    'Edited',
+    'URL',
+  ];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {tableHeaders.map((tableHeadName) => (
+            <th key={ tableHeadName }>
+              {tableHeadName}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {planets && planets.map((planet:Planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
