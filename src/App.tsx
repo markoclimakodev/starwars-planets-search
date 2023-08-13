328ce9cb1419ea9d25415b6a5159d3508b22d4a4
markoclimako <mkclimako@gmail.com>
2023-08-13 01:34:25 -0300

    rebase: reset project for practice
    I am resetting the project to its original state for practice.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,7 @@
-import { useEffect, useState } from 'react';
-import { fetchPlanets } from './Api/fetchPlanets';
 import './App.css';
-import FilterPlanets from './components/FilterPlanets';
-import Table from './components/Table';
-import { PlanetsProvider } from './context/planetsProvider';
-import { Planet } from './types';
 
 function App() {
-  const [planets, setPlanets] = useState<Planet[]>([]);
-  const [filteredPlanets, setFilteredPlanets] = useState<Planet[]>([]);
-
-  const handleFilterPlanet = (searchTerm:string) => {
-    const filtered = planets
-      .filter((planet) => planet.name.includes(searchTerm));
-
-    setFilteredPlanets(filtered);
-  };
-
-  const handleFilteredPlanetNumeric = (array:Planet[]) => {
-    setFilteredPlanets(array);
-  };
-
-  useEffect(() => {
-    const fetchApi = async () => {
-      const planetsResponse = await fetchPlanets();
-      setPlanets(planetsResponse);
-    };
-    fetchApi();
-  }, []);
-
-  const providerValues = {
-    planets,
-    filteredPlanets,
-    handleFilterPlanet,
-    handleFilteredPlanetNumeric,
-  };
-
-  return (
-    <PlanetsProvider value={ providerValues }>
-      <FilterPlanets />
-      <Table />
-    </PlanetsProvider>
-  );
+  return <h1>Hi</h1>;
 }
 
 export default App;
