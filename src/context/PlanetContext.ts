bd3f60a93d779b93fffa403b72bcba7e571926a0
markoclimako <mkclimako@gmail.com>
2023-08-13 01:54:04 -0300

    feat: implement PlanetContext

---
+++ src/context/PlanetContext.ts
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+import { Filter, Planet } from '../types';
+
+export type PlanetContextType = {
+  planets: Planet[],
+  filteredPlanets: Planet[],
+  activeFilters: Filter[],
+  handleFilterPlanets: (planets:Planet[]) => void
+  handleActiveFilters: (filters:Filter[]) => void
+};
+
+export const PlanetContext = createContext({} as PlanetContextType);
