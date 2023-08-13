e4ed8bb5e6b97154ef9691a35f1cbe054d771c2e
markoclimako <mkclimako@gmail.com>
2023-08-13 01:51:47 -0300

    types: add TableColumns, Comparison, and Filter types

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,28 @@ export type Planet = {
   edited: string;
   url: string;
 };
+
+export type TableColumns = {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  films: string;
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export type Comparison = 'maior que' | 'menor que' | 'igual a';
+
+export type Filter = {
+  column: keyof Planet;
+  comparison: Comparison
+  value: string;
+  id: string;
+};
