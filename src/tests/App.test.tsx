8904af3e703fa9027b4d9196fb8d693af6405ca3
markoclimako <mkclimako@gmail.com>
2023-08-13 17:32:06 -0300

    test: increase code coverage up to 90%

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -6,6 +6,7 @@ import { vi } from 'vitest';
 import App from '../App';
 import { checkIfTableIsPresent } from './helpers/helperFunctions';
 import { mockData } from './helpers/mockData';
+import { ascPopulationSort } from './helpers/sortData';
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -33,6 +34,34 @@ describe('Test for Table component', () => {
     const tatooine = await screen.findByRole('cell', { name: /tatooine/i })
 
     expect(tatooine).toBeInTheDocument()
+  })
+  it('the user should be able to filter a planet by term', async () => {
+    const sortColumns = screen.getByTestId('column-sort')
+    const ascSort = screen.getByTestId('column-sort-input-asc')
+    const descSort = screen.getByTestId('column-sort-input-desc')
+    const sortButton = screen.getByTestId('column-sort-button')
+
+ 
+    expect(sortColumns).toBeInTheDocument()
+    expect(ascSort).toBeInTheDocument()
+    expect(descSort).toBeInTheDocument()
+
+    const sortOrder = {
+      column: 'population',
+      order: 'ASC'
+    }
+
+    await userEvent.selectOptions(sortColumns, sortOrder.column)
+    await userEvent.click(ascSort)
+    await userEvent.click(sortButton)
+
+    ascPopulationSort.forEach(async (planetName,index) => {
+      const planet = await screen.findByRole('cell', { name: planetName })
+      expect(planet).toBeInTheDocument()
+      const getAllPlanets = screen.getAllByTestId('planet-name')
+      expect(planet).toEqual(getAllPlanets[index])
+    })
+
   })
 
 });
---
+++ src/tests/helpers/sortData.ts
@@ -0,0 +1,12 @@
+export const ascPopulationSort = [
+  "Yavin IV",
+  "Tatooine",
+  "Bespin",
+  "Endor",
+  "Kamino",
+  "Alderaan",
+  "Naboo",
+  "Coruscant",
+  "Hoth",
+  "Dagobah"
+]
